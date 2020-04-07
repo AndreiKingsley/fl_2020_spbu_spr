f6af66d06d68be88a16e8850dd5bdbfd2a8c4d9e
Andrew Kislitsyn <kisandy@yandex.ru>
2020-04-07 13:46:12 +0300

    compelte lang

--- src/LLang.hs
+++ src/LLang.hs
@@ -1,7 +1,86 @@
 module LLang where
 
 import AST (AST (..), Operator (..))
-import Combinators (Parser (..))
+import Combinators 
+import Control.Applicative
+import Expr
+
+lDigits :: [String]
+lDigits = ["$NOL$", "$CELKOVIY$", "$POLUSHKA$", "$CHETVERTUSHKA$", "$OSMUSHKA$", "$PUDOVICHOK$", "$MEDYACHOK$", "$SEREBRYACHOK$", "$ZOLOTNICHOK$", "$DEVYATICHOK$"]
+
+
+lDigitToInt :: String -> Int
+lDigitToInt x = case x of
+	"$NOL$" -> 0
+	"$CELKOVIY$" -> 1
+	"$POLUSHKA$" -> 2
+   	"$CHETVERTUSHKA$" -> 3
+	"$OSMUSHKA$" -> 4
+	"$PUDOVICHOK$" -> 5
+	"$MEDYACHOK$" -> 6
+	"$SEREBRYACHOK$" -> 7
+	"$ZOLOTNICHOK$" -> 8
+	"$DEVYATICHOK$" -> 9
+
+parseLNum :: Parser String String Int
+parseLNum = foldl (\acc d -> 10 * acc + lDigitToInt d) 0 <$> some (dictParser lDigits)
+
+
+lAlph :: [Char]
+lAlph = ['r', 'u', 's', 'R', 'U', 'S']
+
+parserus ::Parser String String Char
+parserus = satisfy $ \x -> elem x ['r', 'u', 's']
+
+parseRUS ::Parser String String Char
+parseRUS = satisfy $ \x -> elem x ['R', 'U', 'S']
+
+parseLIdent :: Parser String String String
+parseLIdent = do
+	symbol '@'
+        s1 <- many parserus
+        s2 <- many parseRUS
+	symbol '@'
+	return $ s1 ++ s2
+
+
+parseLExpr :: Parser String String AST
+parseLExpr = do
+	symbol ':'
+	ast <- helper
+	symbol ':'
+        return ast
+
+
+
+helper = uberExpr [
+                      (orParser, Binary RightAssoc),
+                      (andParser, Binary RightAssoc),
+                      (notParser, Unary),
+                      (equalParser <|> nequalParser <|> leParser <|> geParser <|> gtParser <|> ltParser, Binary NoAssoc),
+                      (plusParser <|> minusParser, Binary LeftAssoc),
+                      (multParser <|> divParser, Binary LeftAssoc),
+		      (minusParser, Unary),
+                      (powParser, Binary RightAssoc)
+                     ]
+                     (Num <$> parseLNum <|> Ident <$> parseLIdent <|> symbol '(' *> parseLExpr <* symbol ')')
+                     BinOp UnaryOp
+	where 
+		plusParser    = stringParser "+" >>= toOperator
+		multParser    = stringParser "*" >>= toOperator
+		minusParser   = stringParser "-" >>= toOperator
+		divParser     = stringParser "/" >>= toOperator
+		powParser     = stringParser "^" >>= toOperator
+		equalParser   = stringParser "==" >>= toOperator
+		nequalParser  = stringParser "/=" >>= toOperator
+		gtParser      = stringParser ">" >>= toOperator
+		ltParser      = stringParser "<" >>= toOperator
+		geParser      = stringParser ">=" >>= toOperator
+		leParser      = stringParser "<=" >>= toOperator
+		andParser     = stringParser "&&" >>= toOperator
+		orParser      = stringParser "||" >>= toOperator
+		notParser     = stringParser "!" >>= toOperator
+
 
 type Expr = AST
 
@@ -14,22 +93,79 @@ data LAst
   | Read { var :: Var }
   | Write { expr :: Expr }
   | Seq { statements :: [LAst] }
+  | Empty 
   deriving (Show, Eq)
 
-stmt :: LAst
-stmt =
-  Seq
-    [ Read "X"
-    , If (BinOp Gt (Ident "X") (Num 13))
-         (Write (Ident "X"))
-         (While (BinOp Lt (Ident "X") (Num 42))
-                (Seq [ Assign "X"
-                        (BinOp Mult (Ident "X") (Num 7))
-                     , Write (Ident "X")
-                     ]
-                )
-         )
-    ]
+
+
+parseIf :: Parser String String LAst
+parseIf = do
+	symbol '{'
+	stringParser "#KOLI#"
+    	expr <- parseLExpr
+    	stringParser "#TADI#"
+   	block1 <- parseBlock
+    	stringParser "#PO-INOMU#"
+    	block2 <- parseBlock
+	symbol '}'
+    	return If {cond = expr, thn = block1, els = block2}
+
+parseWhile :: Parser String String LAst
+parseWhile = do
+  	symbol '{'
+	stringParser "#PAKUL#"
+   	expr <- parseLExpr
+  	block <- parseBlock
+	symbol '}'	
+  	return While {cond = expr, body = block}
+
+parseAssign :: Parser String String LAst
+parseAssign = do
+	symbol '{'
+    	stringParser "#ZVYAZATI#"
+    	ident <- parseLIdent
+    	expr <- parseLExpr
+	symbol '}'
+   	return Assign {var = ident, expr = expr}
+
+
+parseRead :: Parser String String LAst
+parseRead = do
+	symbol '{'
+    	stringParser "#CHITATSBERESTI#"
+    	ident <- parseLIdent
+	symbol '}'
+    	return Read {var = ident}
+
+
+parseWrite :: Parser String String LAst
+parseWrite = do
+	symbol '{'
+    	stringParser "#NAPISATNABERESTU#"
+    	expr <- parseLExpr
+	symbol '}'
+    	return Write {expr = expr}
+
+
+parseSeq :: Parser String String LAst
+parseSeq = do
+    	symbol '{'
+	stringParser "#ROBIT#"
+    	statements <- many $ parseBlock
+	symbol '}'
+    	return Seq {statements = statements}
+
+parseEmpty :: Parser String String LAst
+parseEmpty = do
+    	symbol '{'
+	stringParser "#PUSTO#"
+	symbol '}'
+    	return Empty
+
+
+parseBlock :: Parser String String LAst
+parseBlock = parseIf <|> parseWhile <|> parseAssign <|> parseRead <|> parseWrite <|> parseSeq <|> parseEmpty
+
 
 parseL :: Parser String String LAst
-parseL = error "parseL undefined"
+parseL = parseSeq
---
+++ test/Test/LLang.hs
@@ -0,0 +1,46 @@
+module Test.LLang where
+
+import           AST                 (AST (..), Operator (..))
+import           Combinators         (Parser (..), Result (..), runParser,
+                                      symbol, stringParser)
+import           Control.Applicative ((<|>))
+import           Expr                (Associativity (..), evaluate, parseExpr,
+                                      parseNum, parseOp, toOperator, uberExpr, parseIdent, OpType (..))
+import           Test.Tasty.HUnit    (Assertion, (@?=), assertBool)
+import		 LLang		     
+
+isFailure (Failure _) = True
+isFailure  _          = False
+
+unit_parseLNum :: Assertion
+unit_parseLNum = do
+    runParser parseLNum "$SEREBRYACHOK$" @?= Success "" 7
+    runParser parseLNum "$CELKOVIY$$NOL$" @?= Success "" 10
+    runParser parseLNum "$POLUSHKA$+$POLUSHKA$" @?= Success "+$POLUSHKA$" 2
+    assertBool "" $ isFailure (runParser parseLNum "4")
+    assertBool "" $ isFailure (runParser parseLNum "DEVYATICHOK")
+
+unit_parseLIdent :: Assertion
+unit_parseLIdent = do
+    runParser parseLIdent "@uRSU@" @?= Success "" "uRSU"
+    runParser parseLIdent "@ussuu@" @?= Success "" "ussuu"
+    runParser parseLIdent "@@tox" @?= Success "tox" ""
+    assertBool "" $ isFailure (runParser parseLNum "@r")
+    assertBool "" $ isFailure (runParser parseLNum "urs")
+    assertBool "" $ isFailure (runParser parseLNum "@SurS@")
+    assertBool "" $ isFailure (runParser parseLNum "@aSS@")
+
+
+unit_parseLExpr :: Assertion
+unit_parseLExpr = do
+    runParser parseLExpr ":$CELKOVIY$*$POLUSHKA$*$CHETVERTUSHKA$:"   @?= Success "" (BinOp Mult (BinOp Mult (Num 1) (Num 2)) (Num 3))
+    runParser parseLExpr ":$CELKOVIY$$POLUSHKA$$CHETVERTUSHKA$:"     @?= Success "" (Num 123)
+    runParser parseLExpr ":@uRR@:"     @?= Success "" (Ident "uRR")
+    runParser parseLExpr ":$CELKOVIY$*$POLUSHKA$+$CHETVERTUSHKA$*$OSMUSHKA$:" @?= Success "" (BinOp Plus (BinOp Mult (Num 1) (Num 2)) (BinOp Mult (Num 3) (Num 4)))
+    runParser parseLExpr ":$CELKOVIY$*@R@*$CHETVERTUSHKA$:"   @?= Success "" (BinOp Mult (BinOp Mult (Num 1) (Ident "R")) (Num 3))
+    assertBool "" $ isFailure (runParser parseLExpr "$CELKOVIY$+$CELKOVIY$")
+
+
+unit_parseL :: Assertion
+unit_parseL = do
+   runParser parseL "{#ROBIT#{#PUSTO#}}" @?= Success "" Seq {statements = [Empty]}
