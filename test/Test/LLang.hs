6ef1a5e5984b00da85c700675f7ec9ceb4e38b07
Andrew Kislitsyn <kisandy@yandex.ru>
2020-04-09 13:42:03 +0300

    fixes

--- test/Test/LLang.hs
+++ test/Test/LLang.hs
@@ -46,3 +46,11 @@ unit_parseL = do
    runParser parseL "{#ROBIT#{#PUSTO#}}" @?= Success "" Seq {statements = [Empty]}
    runParser parseL "{#ROBIT#{#ZVYAZATI#@ruSS@:$CELKOVIY$*$POLUSHKA$*$CHETVERTUSHKA$:}{#PAKUL#:$NOL$:{#ROBIT#{#NAPISATNABERESTU#:-@ruSS@:}}}}"
 	@?= Success "" Seq {statements = [Assign {var = "ruSS", expr = (BinOp Mult (BinOp Mult (Num 1) (Num 2)) (Num 3))}, While {cond = (Num 0), body = Seq{statements = [Write {expr = (UnaryOp Minus (Ident "ruSS"))}]}}]}
+   runParser parseL "{#ROBIT#{#ZVYAZATI#@USSR@:$CELKOVIY$$DEVYATICHOK$$DEVYATICHOK$$CELKOVIY$+-$CELKOVIY$$DEVYATICHOK$$POLUSHKA$$POLUSHKA$:}}" 
+	@?= Success "" Seq {statements = [Assign {var = "USSR", expr = (BinOp Plus (Num 1991) (UnaryOp Minus (Num 1922)))}]}
+   runParser parseL "{#ROBIT#{#KOLI#:@USSR@>$CELKOVIY$$NOL$$NOL$:#TADI#{#ZVYAZATI#@USSR@:$CELKOVIY$+$NOL$-$NOL$:}#PO-INOMU#{#PUSTO#}}}" 
+	@?= Success "" Seq {statements = [If {cond = BinOp Gt (Ident "USSR") (Num 100), thn = Assign {var = "USSR", expr = BinOp Minus (BinOp Plus (Num 1)(Num 0)) (Num 0)}, els = Empty}]}
+   runParser parseL "{#ROBIT#{#PAKUL#:@USSR@:{#NAPISATNABERESTU#:$ZOLOTNICHOK$:}}}" 
+	@?= Success "" Seq {statements = [While {cond = (Ident "USSR"), body = Write {expr = (Num 8)}}]}
+   runParser parseL "{#ROBIT#{#ZVYAZATI#@USSR@:$CELKOVIY$$DEVYATICHOK$$DEVYATICHOK$$CELKOVIY$+-$CELKOVIY$$DEVYATICHOK$$POLUSHKA$$POLUSHKA$:}{#KOLI#:@USSR@>$CELKOVIY$$NOL$$NOL$:#TADI#{#ZVYAZATI#@USSR@:$CELKOVIY$+$NOL$-$NOL$:}#PO-INOMU#{#PUSTO#}}{#PAKUL#:@USSR@:{#NAPISATNABERESTU#:$ZOLOTNICHOK$:}}}" 
+	@?= Success "" Seq {statements = [Assign {var = "USSR", expr = (BinOp Plus (Num 1991) (UnaryOp Minus (Num 1922)))}, If {cond = BinOp Gt (Ident "USSR") (Num 100), thn = Assign {var = "USSR", expr = BinOp Minus (BinOp Plus (Num 1)(Num 0)) (Num 0)}, els = Empty}, While {cond = (Ident "USSR"), body = Write {expr = (Num 8)}}]}
