db8eace4fdc6239716c915b1a88528aa56ad321f
Andrew Kislitsyn <kisandy@yandex.ru>
2020-05-13 19:49:25 +0300

    1

--- src/Regexp.hs
+++ src/Regexp.hs
@@ -8,13 +8,25 @@ data Regexp = Empty
             | Seq Regexp Regexp
             | Alt Regexp Regexp
             | Star Regexp
-            deriving (Eq, Ord)
+            deriving (Show, Eq, Ord)
 
 match :: Regexp -> String -> Bool
 match r s = nullable (foldl (flip derivative) r s)
 
 derivative :: Char -> Regexp -> Regexp
-derivative = error "derivative not implemented"
+derivative a Empty = Empty
+derivative a Epsilon = Empty
+derivative a (Char b) | a /= b = Empty
+                      | otherwise = Epsilon
+derivative a (Seq r1 r2) | nullable r1 = Alt (Seq (derivative a r1) r2) (derivative a r2)
+                         | otherwise = Seq (derivative a r1) r2
+derivative a (Alt r1 r2) = Alt (derivative a r1) (derivative a r2)
+derivative a sr@(Star r) = Seq (derivative a r) sr
 
 nullable :: Regexp -> Bool
-nullable = error "nullable not implemented"
+nullable Empty = False
+nullable Epsilon = True
+nullable (Char a) = False
+nullable (Seq r1 r2) = (nullable r1) && (nullable r2)
+nullable (Alt r1 r2) = (nullable r1) || (nullable r2)
+nullable (Star r)= True
