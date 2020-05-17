a301ac0d157fb34c6535fe553e5ea9a670452277
Andrew Kislitsyn <kisandy@yandex.ru>
2020-05-17 11:57:00 +0300

    better output

--- python_parser/main.py
+++ python_parser/main.py
@@ -1,13 +1,21 @@
 from yacc_parser import parse
 
+
+def list_concat(l):
+    ans = ''
+    for elem in l:
+        ans += elem
+    return ans
+
+
 if __name__ == '__main__':
     file_name = input()
     f = open(file_name, "r")
     txt = f.read()
     term_list, nonterm_list, rules, error = parse(txt)
-    print('Terminals:', ', '.join(term_list))
-    print('Nonterminals:', ', '.join(nonterm_list))
+    print('Terminals:', ', '.join(sorted(list(term_list))))
+    print('Nonterminals:', ', '.join(sorted(list(nonterm_list))))
     print('Rules:')
     for lhs, rhs in rules.items():
-        print(' *', lhs, '->', ' | '.join(rhs))
+        print(' *', lhs, '->', ' | '.join(list(map(lambda x: list_concat(x), rhs))))
     f.close()
